596d4c560b3e35eaf53f8ace95f9e4b6219db390
gustavo panizzo <gfa@zumbi.com.ar>
2025-08-10 16:39:15 +0200

    update hiera-eyaml to 3.4.0

--- pkgs/by-name/hi/hiera-eyaml/gemset.nix
+++ pkgs/by-name/hi/hiera-eyaml/gemset.nix
@@ -8,27 +8,27 @@
     platforms = [ ];
     source = {
       remotes = [ "https://rubygems.org" ];
-      sha256 = "049rxnwyivqgyjl0sjg7cb2q44ic0wsml288caspd1ps8v31gl18";
+      sha256 = "19eba2ea9d70b2d56de064df9e3242860d8070d47ff9c9a6348b7fe94708ab0a";
       type = "gem";
     };
-    version = "3.0.0";
+    version = "4.3.0";
   };
   highline = {
     source = {
       remotes = [ "https://rubygems.org" ];
-      sha256 = "06bml1fjsnrhd956wqq5k3w8cyd09rv1vixdpa3zzkl6xs72jdn1";
+      sha256 = "67cbd34d19f6ef11a7ee1d82ffab5d36dfd5b3be861f450fc1716c7125f4bb4a";
       type = "gem";
     };
-    version = "1.6.21";
+    version = "3.1.2";
   };
   optimist = {
     groups = [ "default" ];
     platforms = [ ];
     source = {
       remotes = [ "https://rubygems.org" ];
-      sha256 = "05jxrp3nbn5iilc1k7ir90mfnwc5abc9h78s5rpm3qafwqxvcj4j";
+      sha256 = "81886f53ee8919f330aa30076d320d88eef9bc85aae2275376b4afb007c69260";
       type = "gem";
     };
-    version = "3.0.0";
+    version = "3.1.0";
   };
 }
